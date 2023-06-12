45c6970564fbe6f73cf9cdd7aac0742910247c5a
Luna <lunahatesgogle@gmail.com>
2023-06-12 20:17:03 -0300

    lets bumpppppppp oh yeah

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+[ -d /server/plugins ] && rm -f /server/plugins/*.jar
+
 rsync --chown server:server --remove-source-files -rltd  /server_tomerge/ /server/
 chown -R server:server /server
 
